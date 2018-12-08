74b069b51e7283d9d539da7ac3023de88c12a893
andy.rothwell <rothwell.andy@gmail.com>
2018-12-08 08:48:56 -0500

    removes Division.vue

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 
 import Badge from './components/Badge.vue';
 import ExternalLink from './components/ExternalLink.vue';
-// import Division from './components/Division.vue';
 // import Callout from './components/Callout.vue';
 import TopicComponent from './components/TopicComponent.vue';
 
 export {
   Badge,
-  // Division,
   ExternalLink,
   // Callout,
   TopicComponent
