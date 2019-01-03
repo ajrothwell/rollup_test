110257e79ad9511f37dc451fbd38cca31ecbcfd5
andy.rothwell <rothwell.andy@gmail.com>
2019-01-03 16:39:24 -0500

    testing using index.js

---
+++ src/components/index.js
@@ -0,0 +1,35 @@
+
+export { default as Badge } from './Badge.vue';
+export { default as Callout } from './Callout.vue';
+export { default as ExternalLink } from './ExternalLink.vue';
+export { default as TopicComponent } from './TopicComponent.vue';
+
+// import Vue from "vue";
+// import TopicComponent from './TopicComponent.vue';
+// import ExternalLink from './ExternalLink.vue';
+// import Badge from './Badge.vue';
+// import Test from './Test.vue';
+//
+//
+// const Components = {
+//   TopicComponent,
+//   ExternalLink,
+//   Badge,
+//   Test
+// }
+//
+// // Object.keys(Components).forEach(name => {
+// //   Vue.component(name, Components[name]);
+// // })
+//
+// export default Components;
+
+// export TopicComponent;
+// export ExternalLink;
+// export Badge;
+// export Test;
+
+// export { TopicComponent };
+// export { ExternalLink };
+// export { Badge };
+// export { Test };
